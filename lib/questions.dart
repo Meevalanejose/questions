524c7d31cf5b36004c09f619e77f052d48525896
Sourravk <souravk2366@gmail.com>
2024-02-20 16:58:58 +0530

    initial commit

--- lib/questions.dart
+++ lib/questions.dart
@@ -11,6 +11,49 @@ class _QuestionsState extends State<Questions> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+        mainAxisAlignment:MainAxisAlignment.center ,
+        children:
+        [
+          Text('ERTYUIODFGHJKLDFGHJKFGHJKL',
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight:FontWeight.bold,
+              color: Colors.white,
+            ),),
+          SizedBox(
+            height: 30,
+          ),
+          ElevatedButton(style: ElevatedButton.styleFrom(primary: Colors.green,
+          ),
+
+
+            onPressed: () {  },
+            child: Text('TRUE',style: TextStyle(
+              color: Colors.brown,
+              fontSize: 30,
+              fontWeight:FontWeight.bold
+
+            ),),
+
+            ),
+          SizedBox(
+
+            height: 30,
+          ),
+          ElevatedButton(style: ElevatedButton.styleFrom(primary: Colors.red,),
+
+              onPressed: () {  },
+              child: Text('FALSE',style: TextStyle(
+                fontSize: 30,
+                fontWeight:FontWeight.bold),),
+
+          ),
+        ],
+      ),
+      ),
+
     );
   }
 }
